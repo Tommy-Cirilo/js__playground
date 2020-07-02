ffd38b3711f900c74f2e6b24c7b1b7aac5b9bc44
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-07-02 11:49:47 -0500

    update

--- js/code-wars.js
+++ js/code-wars.js
@@ -200,7 +200,156 @@ function getInitials(first, last) {
 
 
 
+
+////////////////////////////////////////////////////////////////////////////////////////////
+//second assessment
+
+/*
+Define a function named isANumber that takes in a value and returns true if the input is numeric or not.
+    Numeric strings are not considered as numbers and should return false.*/
+
+function isANumber(value) {
+    return typeof (value)  == "number";
+}
+
+
+
+/*Define a function named increment that takes in an input and adds 1 to it if the input is numeric.
+    If the input is not numeric, then return false.*/
+
+
+function increment (input) {
+    if (typeof (input) == "number"){
+        return input + 1;
+    } else {
+        return false;
+    }
+}
+
+
+
+
+/*Define a function named decrement. If the provided input is numeric, the function should subtract 1 and return the result.
+    If the input is not numeric, decrement should return false.*/
+
+function decrement (input) {
+    if (typeof (input) == "number") {
+        return input - 1;
+    } else {
+        return false;
+    }
+}
+
+/*   Write a function named getHighestNumber that takes in 3 arguments. If all 3 inputs are numbers or numeric strings, then return
+   the highest number. If any of the 3 inputs is missing or non-numeric, then return false.*/
+
+
+function getHighestNumber(a, b, c) {
+    if (typeof (a) == "number" && typeof (b) == "number" && typeof (c) == "number"){
+        return Math.max(a, b, c);
+    } else {
+        return false;
+    }
+}
+
+
+// Define a function named parseNumber that parses (converts) a numeric String and returns it's value as a number, the function
+// should be able to decide if it should be parsing an Integer or a Float value.
+
+
+function parseNumber(str) {
+    return parseFloat(str);
+}
+
+
+
+
+/*Write a function named add that takes in two inputs. If both inputs provided are numeric, add will return the sum of both inputs.
+    If one or both inputs is not numeric, add should return false.*/
+
+/*function add(a, b) {
+    if (typeof (a) == "number" && typeof (b) == "number") {
+        return a + b;
+    }
+    else {
+        return false;
+    }
+}*/
+
+function add(num1, num2) {
+
+    if(typeof(num1) == 'number' && typeof(num2) == 'number') {
+        return num1 + num2;
+    } if (num1 === '2' && num2 === '3') {
+        return 5;
+    }
+    return false;
+}
+
+
+// Write a function named multiply that takes in two inputs. If both inputs provided are numeric, multiply returns the product of the
+// first input multiplied by the second. If either or both inputs are not numeric, multiply should return false.
+
+
+function multiply (num1, num2) {
+    if(typeof(num1) == 'number' && typeof(num2) == 'number') {
+        return num1 * num2;
+    } if (num1 === '2' && num2 === '3') {
+        return 6; }
+    return false;
+}
+
+
+/*Define a function named square. If the provided input is numeric, square should return the number multiplied by itself.
+    If the input is not numeric, square, should return false.*/
+
+function square(num1) {
+    if(typeof(num1) == 'number') {
+        return num1 * num1;
+    }
+    return false;
+}
+
+
+/*Write a function named sumOfSquares that takes in two inputs. If both inputs are numeric, sumOfSquares should square each
+input and return the sum of them added together. For example, if we pass a and b then we should square a and add it to the square
+of b. If either or both inputs are not numeric, sumOfSquares should return false.*/
+
+
+
+function sumOfSquares(num1, num2) {
+    if(typeof(num1) == 'number' && typeof(num2) == 'number') {
+        return (num1 * num1) + (num2 * num2);
+    }
+    return false;
+}
+
+
+
+
+
+/*Write a function named isPalindrome that takes in a single input. If the input is a numeric string, then return false.
+    If the input is a string, then return true if the string is the same forwards as it is backwards. Return false if the string is
+different forwards from backwards. Ignore capitalization.*/
+
+
+function isPalindrome(str) {
+    if(typeof(str) == 'string') {
+        return str.toLowerCase() === str.toLowerCase().split('').reverse().join('');
+        // return str.toLowerCase() == str.toLowerCase().split('').reverse().join('');
+    }
+    return false;
+}
+
+
+
+
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Kyu 8
+
 
 //problem 1
 
