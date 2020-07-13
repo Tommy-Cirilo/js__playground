3261314621afb1ab00fe48e78d0c52f2fc5828bc
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-07-13 15:35:54 -0500

    update new exercise

--- js/code-wars.js
+++ js/code-wars.js
@@ -471,5 +471,41 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
 
 
+/*7. Reverse Integer
+
+Given a 32-bit signed integer, reverse digits of an integer.
+
+    Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+Note:
+    Assume we are dealing with an environment which could only store integers within
+    the 32-bit signed integer range: [−231,  231 − 1].
+For the purpose of this problem, assume that your function returns 0 when the
+reversed integer overflows.*/
+
+
+var reverse = function(x) {
+    return (
+        parseFloat(
+            x
+                .toString()
+                .split('')
+                .reverse()
+                .join('')
+        ) * Math.sign(x)
+    )
+};
+
+
 
 
