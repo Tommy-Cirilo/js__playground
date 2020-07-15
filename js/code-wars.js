2873180fc6a4eb23f83cb809b7b0d699f0e316e2
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-07-15 12:34:33 -0500

    update

--- js/code-wars.js
+++ js/code-wars.js
@@ -732,3 +732,47 @@ const findUsers = function (usersArray) {
 
 console.log(findUsers()); //['sophie, vivian, david']
 
+
+
+
+// ---------------------------------------------------------------------------------------------------------------
+
+// Define a function `reverseArrayString` that accepts one argument,
+// a string of comma separated values. It should reverse the order of the values and return a string.
+// Eg. reverseArrayString("1,2,3") should return "3,2,1"
+
+
+
+// Define a function `swapFirstLast` that accepts one argument, an array. It should take the first
+// and last elements, and swap their positions in the array.
+// Eg. swapFirstLast([0,2,4]) should return [4,2,0]
+
+
+
+// Define a function `chopDNA` that accepts one argument, an array of strings. It should iterate the
+// array, if the element is the stop codon "TAG", chop off the rest of the array and return it.
+// Eg. chopDNA(["CCC", "AGG", "TAA", "CAA"]) should return ["CCC", "AGG", "TAA"]
+
+
+
+// Define a function `sortByViews` that accepts one argument, an array of objects. It should iterate
+// the array and sort the objects by their `views` property.
+
+
+
+// TODO This array of objects is just for reference, you can leave it commented
+// let videos = [
+//   {
+//     name: "Keyboard Cat",
+//     views: 59000000
+//   },
+//   {
+//     name: "Charlie Bit My Finger",
+//     views: 875000000
+//   },
+//   {
+//     name: "Rick Astley - Never Gonna Give You Up",
+//     views: 721000000
+//   },
+// ];
+
