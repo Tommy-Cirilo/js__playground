16679bcc74755c84776ed73fb79f75a8530b38bc
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-22 23:23:20 -0500

    New fun exercises to work with

--- js/code-wars.js
+++ js/code-wars.js
@@ -675,6 +675,8 @@ function evensIndex(arr) {
 //
 // stringToArray("1,2,3") // returns ['1','2','3']
 // stringToArray("dog") // returns ['dog']
+
+
 // Given the following array of objects, write a function 'findUsers' that returns an Array of usernames.
 //     var myMac = {};
 // myMac.users = [
